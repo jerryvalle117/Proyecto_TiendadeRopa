65464abc62ffe49bd046f66cee76c434450540a7
Jerry Valle <marce@DESKTOP-76F7GM8>
2021-02-12 23:26:40 -0600

    Agregamos arquitectura basica

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -1,9 +1,10 @@
-﻿using QuieroPizza.Web.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TiendadeRopa.BL;
 
 namespace QuieroPizza.Web.Controllers
 {
@@ -12,23 +13,8 @@ namespace QuieroPizza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza 6 quesos";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pizza 3 estaciones";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pizza peperoni y hongos";
-
-            var listadeProductos = new List<ProductoModel>();
-
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
 
             return View(listadeProductos);
---
+++ QuieroPizza/TiendadeRopa.BL/ProductosBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendadeRopa.BL
+{
+    public class ProductosBL
+    {
+       public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Camisa Pepe color rojo";
+            producto1.Precio = 200;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "pantalon jean Pepe azul ";
+            producto2.Precio = 700;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Tenis Nike f50 negros";
+            producto3.Precio = 1200;
+
+            var listadeProductos = new List<Producto>();
+
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            return listadeProductos;
+        }
+    }
+}
