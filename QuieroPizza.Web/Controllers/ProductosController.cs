2c7b653078f96465f2cd8ef85d18dff51cd410d4
marce <marce@DESKTOP-76F7GM8>
2021-03-08 23:28:32 -0600

    Agregar archivos de proyecto.

---
+++ QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendadeRopa.BL;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+
+
+            return View(listadeProductos);
+        }
+    }
+}
